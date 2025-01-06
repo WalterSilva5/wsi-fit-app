0ab243ea52e114e01a20b4892dca9fc38f16217e
waltersilva5 <waltersilva5@outlook.com>
2025-01-05 22:23:53 -0300

    feature user create form ts

--- src/app/pages/user/forms/create/user-create.component.ts
+++ src/app/pages/user/forms/create/user-create.component.ts
@@ -1,16 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { UserDataService } from '../../data/data.service';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AsyncPipe, CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user-create',
   standalone: true,
   templateUrl: './user-create.component.html',
-  styleUrl: './user-create.component.scss',
+  styleUrls: ['./user-create.component.scss'],
+  imports: [
+    ReactiveFormsModule,
+    CommonModule,
+    AsyncPipe,
+  ],
 })
 export class UserCreateComponent implements OnInit {
-  constructor(private api: UserDataService) {}
+  form: FormGroup = new FormGroup({});
+
+  constructor(private fb: FormBuilder, private api: UserDataService) {}
+
   ngOnInit(): void {
-    console.log('UserCreateComponent');
+    this.form = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      role: ['USER', Validators.required],
+    });
   }
 
+  onSubmit() {
+    if (this.form.valid) {
+      console.log('Form values:', this.form.value);
+    }
+  }
 }
