96e58fca80309f9cb20882c5afa003dc855fc43b
waltersilva5 <waltersilva5@outlook.com>
2025-01-26 22:42:59 -0300

    style: user create form

--- src/app/pages/user/forms/create/user-create.component.ts
+++ src/app/pages/user/forms/create/user-create.component.ts
@@ -1,24 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { UserDataService } from '../../data/data.service';
-import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  ReactiveFormsModule,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { AsyncPipe, CommonModule } from '@angular/common';
 import Swal from 'sweetalert2';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-user-create',
   standalone: true,
   templateUrl: './user-create.component.html',
   styleUrls: ['./user-create.component.scss'],
-  imports: [
-    ReactiveFormsModule,
-    CommonModule,
-    AsyncPipe,
-  ],
+  imports: [ReactiveFormsModule, CommonModule, AsyncPipe, RouterModule],
 })
 export class UserCreateComponent implements OnInit {
   form: FormGroup = new FormGroup({});
 
-  constructor(private fb: FormBuilder, private api: UserDataService) {}
+  constructor(
+    private fb: FormBuilder,
+    private api: UserDataService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.form = this.fb.group({
@@ -32,21 +38,24 @@ export class UserCreateComponent implements OnInit {
 
   onSubmit() {
     if (this.form.valid) {
-      this.api.create(this.form.value).then((res) => {
-        console.log('User created: ', res);
-        Swal.fire({
-          icon: 'success',
-          title: 'User created successfully',
-          showConfirmButton: true,
+      this.api
+        .create(this.form.value)
+        .then((res) => {
+          console.log('User created: ', res);
+          Swal.fire({
+            icon: 'success',
+            title: 'User created successfully',
+            showConfirmButton: true,
+          });
         })
-      }).catch((err) => {
-        console.error('Error creating user: ', err);
-        Swal.fire({
-          icon: 'error',
-          title: 'Error creating user',
-          showConfirmButton: true,
-        })
-      });
+        .catch((err) => {
+          console.error('Error creating user: ', err);
+          Swal.fire({
+            icon: 'error',
+            title: 'Error creating user',
+            showConfirmButton: true,
+          });
+        });
     }
   }
 }
